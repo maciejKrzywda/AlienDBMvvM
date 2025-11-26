cf1f9a26ec04b7b675c87cb6329e3f87b4cc2d53
Łukasz Darmofał <legolokskoks@gmail.com>
2025-11-26 20:02:25 +0100

    Create CharacterWindow

--- AlienDBMvvM/Models/CharacterModel.cs
+++ AlienDBMvvM/Models/CharacterModel.cs
@@ -2,14 +2,14 @@ namespace AlienDBMvvM.Models;
 
 public class CharacterModel
 {
-    public string Name { get; set; }
-    public string Surname { get; set; }
-    public string Role { get; set; }
-    public string Actor { get; set; }
-    public string Spiecies { get; set; }
-    public int BirthYear { get; set; }
-    public string Function { get; set; }
-    public string Characteristics { get; set; }
-    public string Fate {get; set;}
-    public string FunFact  { get; set; }
+    public string Name { get; set; } = "brak";
+    public string Surname { get; set; } = "brak";
+    public string Role { get; set; } = "brak";
+    public string Actor { get; set; } = "brak";
+    public string Spiecies { get; set; } = "brak";
+    public int BirthYear { get; set; } = 0;
+    public string Function { get; set; } = "brak";
+    public string Characteristics { get; set; } = "brak";
+    public string Fate {get; set;} = "brak";
+    public string FunFact  { get; set; } = "brak";
 }
--- AlienDBMvvM/Views/DetailWindow.axaml.cs
+++ AlienDBMvvM/Views/DetailWindow.axaml.cs
@@ -1,9 +1,13 @@
+using System;
+using System.Reactive;
+using System.Reactive.Disposables;
 using AlienDBMvvM.Models;
 using AlienDBMvvM.ViewModels;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
+using ReactiveUI;
 
 namespace AlienDBMvvM.Views;
 
@@ -12,5 +16,25 @@ public partial class DetailWindow: ReactiveWindow<DetailWindowViewModel>
     public DetailWindow()
     {
         InitializeComponent();
+
+        ViewModel = new DetailWindowViewModel(new FilmModel());
+        DataContext = ViewModel;
+        
+        this.WhenActivated((CompositeDisposable disposables) =>
+        {
+            ViewModel.ShowCharactersWindow.RegisterHandler(async interaction =>
+                {
+                    var win = new CharacterWindow()
+                    {
+                        DataContext = new CharacterViewModel(interaction.Input)
+                    };
+                
+                    await win.ShowDialog(this);
+                    interaction.SetOutput(Unit.Default);
+                
+                })
+                .DisposeWith(disposables);
+        });
+        
     }
 }
