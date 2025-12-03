6baa42a69c7c09d2fc4a7f37cd29f251a4a2d7d8
Mk <maciej.krzywda@cyfroweszkoly.pl>
2025-12-03 19:48:37 +0100

    usuwanie zadziałało

---
+++ AlienDBMvvM/ViewModels/FilmViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reactive;
+using AlienDBMvvM.Models;
+using ReactiveUI;
+
+namespace AlienDBMvvM.ViewModels;
+
+public class FilmViewModel: ViewModelBase
+{
+    public FilmModel Model { get; }
+    public string Title => Model.Title;
+    public string PolishTitle => Model.PolishTitle;
+    public int ReleaseYear => Model.ReleaseYear;
+    public string Director => Model.Director;
+    public string ScreenWriter => Model.ScreenWriter;
+    public string Genre => Model.Genre;
+    public int Duration => Model.Duration;
+    public double Rating => Model.Rating;
+    public string[] MainCharacters => Model.MainCharacters;
+    public string Ship => Model.Ship;
+    public string Description => Model.Description;
+    public string FunFact => Model.FunFact;
+
+    public ReactiveCommand<Unit, Unit> DeleteCommand { get; }
+    
+    public FilmViewModel(FilmModel model, Action<FilmViewModel> removeAction)
+    {
+        Model = model;
+        
+        DeleteCommand = ReactiveCommand.Create(() => removeAction(this));
+    }
+
+}
